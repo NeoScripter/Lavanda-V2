2f6984aeb2e66a35c7ba150f63d1eb2f7d35b624
Neoscripter <sange0337@gmail.com>
2026-08-15 18:21:10 +0800

    Debug:add envs to the env file

--- php/config/globals.php
+++ php/config/globals.php
@@ -6,4 +6,4 @@ $hive->set('app_name', "Lavanda");
 $hive->set('app_env', getenv('APP_ENV'));
 $hive->set('app_debug', getenv('APP_DEBUG'));
 $hive->set('app_url', getenv('PHP_APP_URL'));
-$hive->set('db_port', getenv('DB_USER'));
+$hive->set('db_port', (int) getenv('DB_PORT'));
