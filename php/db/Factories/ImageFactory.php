123246bd740ac769033635e2fc6ab227ae062ed0
Neoscripter <sange0337@gmail.com>
2026-09-03 16:00:29 +0800

    Refactor:improve and simplify the code of ImageFactory

--- php/db/Factories/ImageFactory.php
+++ php/db/Factories/ImageFactory.php
@@ -5,60 +5,64 @@ declare(strict_types=1);
 namespace Factories;
 
 use Http\Models\Image;
+use InvalidArgumentException;
+use RuntimeException;
 
 class ImageFactory extends Factory
 {
-    public function __construct(public ?string $template = '', ?string $variant = 'front_image')
+    public function create(array $attrs, string $src_dir)
     {
-        $this->template = APP_DIR . "/db/Fixtures/Image/{$variant}/placeholder.png";
-    }
+        if (!is_dir($src_dir)) {
+            throw new InvalidArgumentException('Source directory is not a directory');
+        }
+        if (!array_key_exists('variant', $attrs)) {
+            throw new InvalidArgumentException('No variant provided to the image factory');
+        }
+        if (!array_key_exists('imageable_type', $attrs)) {
+            throw new InvalidArgumentException('No imageable_type provided to the image factory');
+        }
+        if (!array_key_exists('imageable_id', $attrs)) {
+            throw new InvalidArgumentException('No imageable_id provided to the image factory');
+        }
 
-    public function create(string $dir, string $imageable_type, int $imageable_id, ?string $variant = 'image')
-    {
-        $files = $this->get_template_variants();
+        $img_type = $attrs['imageable_type'];
+        $img_id = $attrs['imageable_id'];
+        $variant = $attrs['variant'];
 
-        $new_dir = \UPLOAD_DIR . "$dir/" . uniqid() . '/';
+        $files = read_dir_files($src_dir);
+
+        if (empty($files)) {
+            throw new InvalidArgumentException('Source directory provided to the image factory is empty');
+        }
+
+        $new_dir = \UPLOAD_DIR . "$img_type/" . uniqid() . '/';
 
         if (!is_dir($new_dir)) {
             mkdir($new_dir, 0755, true);
         }
 
-        $this->copy_variants_to_new_directory($new_dir, $files);
+        foreach ($files as $file) {
+            if (!copy($src_dir . '/' . $file, $new_dir . $file)) {
+                throw new RuntimeException("Failed to copy $file to $new_dir");
+            }
+        }
+
+        $raw_matches = preg_grep('/(png|jpg|jpeg)$/', $files);
+
+        if (empty($raw_matches)) {
+            throw new RuntimeException("The original files don't contain png, jpg, or jpeg variant");
+        }
+
+        $raw_file = reset($raw_matches)[0];
 
         $image = new Image();
-        $image->src = to_public_url($new_dir . 'placeholder.png');
-        $image->alt = 'placeholder';
+        $image->src = to_public_url($new_dir . $raw_file);
+        $image->alt = $attrs['alt'] ?? 'image';
         $image->variant = $variant;
-        $image->imageable_id = $imageable_id;
-        $image->imageable_type = $imageable_type;
+        $image->imageable_id = $img_id;
+        $image->imageable_type = $img_type;
         $image->save();
 
         return $image;
-    }
-
-    private function copy_variants_to_new_directory(string $new_dir, array $files)
-    {
-        foreach ($files as $file) {
-            if (!copy($this->template, $new_dir . $file)) {
-                throw new \RuntimeException("Failed to copy $file to $new_dir");
-            }
-        }
-    }
-
-    private function get_template_variants()
-    {
-        if (!file_exists($this->template)) {
-            throw new \RuntimeException("Template image not found: $this->template");
-        }
-
-        $dir = dirname($this->template);
-
-        $files = array_filter(scandir($dir), fn($file) => is_file($dir . '/' . $file));
-
-        if (! $files) {
-            throw new \RuntimeException("Directory $dir is empty");
-        }
-
-        return $files;
     }
 }
