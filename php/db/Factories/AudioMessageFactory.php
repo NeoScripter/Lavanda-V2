ccb14327fc83a8135a216e1123597f8a44b5829d
Neoscripter <sange0337@gmail.com>
2026-09-05 21:45:23 +0800

    Feat:modify AudioMessageSeeder

--- php/db/Factories/AudioMessageFactory.php
+++ php/db/Factories/AudioMessageFactory.php
@@ -8,39 +8,30 @@ use Http\Models\AudioMessage;
 
 class AudioMessageFactory extends Factory
 {
-    public function create()
+    public function create(array $attrs, string $file)
     {
-        $template = APP_DIR . "/db/Fixtures/Audio/audio-1.mp3";
-        $dir = dirname($template);
-
-        $files = array_filter(
-            scandir($dir),
-            fn($file) => is_file($dir . '/' . $file)
-        );
-
-        if (! $files) {
-            throw new \RuntimeException("Directory $dir is empty");
+        if (! isset($attrs['description'])) {
+            throw new \RuntimeException("Description is not provided");
+        }
+        if (! is_file($file)) {
+            throw new \RuntimeException("File $file doesn't exist");
         }
 
-
-        $files = array_values($files);
         $new_dir = \UPLOAD_DIR . '/' . uniqid() . '/';
 
         if (!is_dir($new_dir)) {
             mkdir($new_dir, 0755, true);
         }
 
-        $random_idx = rand(0, count($files) - 1);
-        $from = remove_extra_slashes($dir . '/' . $files[$random_idx]);
-        $to = remove_extra_slashes($new_dir . '/' . $files[$random_idx]);
+        $to = remove_extra_slashes($new_dir . '/' . extract_file_name($file));
 
-        if (!copy(to: $to, from: $from)) {
-            throw new \RuntimeException("Failed to copy $from to $to");
+        if (!copy(to: $to, from: $file)) {
+            throw new \RuntimeException("Failed to copy $file to $to");
         }
 
         $audio = new AudioMessage();
         $audio->file = to_public_url(remove_file_extention($to));
-        $audio->description = 'placeholder';
+        $audio->description = $attrs['description'];
         $audio->save();
 
         return $audio;
--- php/db/Seeders/AudioMessageSeeder.php
+++ php/db/Seeders/AudioMessageSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Seeders;
 
 use Factories\AudioMessageFactory;
+use InvalidArgumentException;
 use Seeders\Seeder;
 
 class AudioMessageSeeder extends Seeder
@@ -16,9 +17,28 @@ class AudioMessageSeeder extends Seeder
             return;
         }
 
+        $source = APP_DIR . '/db/Fixtures/AudioMessage/';
+
+        $fixtures = array_filter(
+            scandir($source),
+            fn($dir) => $dir !== '..' && $dir !== '.' && is_dir($source . '/' . $dir)
+        );
+
+        if (empty($fixtures)) {
+            throw new InvalidArgumentException("Fixtures for audios are not created");
+        }
+
         $factory = new AudioMessageFactory();
-        for ($i = 0; $i < 10; $i++) {
-            $factory->create();
+
+        foreach ($fixtures as $fixture) {
+            $seed_dir = remove_extra_slashes($source . '/' . $fixture . '/');
+
+            $description = read_or_throw($seed_dir . 'description.txt', "Couldn't extract audio description from the file");
+
+            $factory->create(
+                attrs: compact('description'),
+                file: $seed_dir . 'file.mp3',
+            );
         }
 
         echo "Audios seeded.\n";
