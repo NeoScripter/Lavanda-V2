61078de398f05f711f090e2ddbc7d16b8e852b48
Neoscripter <sange0337@gmail.com>
2026-09-06 21:13:27 +0800

    Feat:create variants of the card images

--- php/db/Seeders/CardSeeder.php
+++ php/db/Seeders/CardSeeder.php
@@ -6,6 +6,7 @@ namespace Seeders;
 
 use Enums\CardVariant;
 use Factories\CardFactory;
+use InvalidArgumentException;
 use Seeders\Seeder;
 
 class CardSeeder extends Seeder
@@ -17,14 +18,49 @@ class CardSeeder extends Seeder
             return;
         }
 
-        foreach (CardVariant::values() as $variant) {
+        $source = APP_DIR . '/db/Fixtures/Card/';
 
-            $factory = new CardFactory();
-            for ($i = 0; $i < 10; $i++) {
-                $factory->create(attrs: ['variant' => $variant]);
-            }
+        $variants = array_filter(
+            scandir($source),
+            fn($dir) => $dir !== '..' && $dir !== '.' && is_dir($source . '/' . $dir)
+        );
+
+        if (empty($variants)) {
+            throw new InvalidArgumentException("Card variants are not created");
         }
 
+        $factory = new CardFactory();
+
+        foreach ($variants as $variant) {
+            $variant = CardVariant::from($variant)->value;
+
+            $fixtures = array_filter(
+                scandir($source . '/' . $variant),
+                fn($dir) => $dir !== '..' && $dir !== '.' && is_dir($source . '/' . $dir)
+            );
+
+            if (empty($fixtures)) {
+                throw new InvalidArgumentException("Fixtures for $variant are not created");
+            }
+
+            foreach ($fixtures as $fixture) {
+                $seed_dir = remove_extra_slashes($source . '/' . $variant . '/' . $fixture . '/');
+
+                $name = read_or_throw($seed_dir . 'name.txt', "Couldn't extract card name from the file");
+                $advice = read_or_throw($seed_dir . 'advice.txt', "Couldn't extract card advice from the file");
+                $html = read_or_throw($seed_dir . 'theme.md', "Couldn't extract card theme from the file");
+                $description = 'Описание карты';
+
+                $card = $factory->create(
+                    attrs: compact('name', 'advice', 'description'),
+                    img_src: $seed_dir . 'front_image/',
+                );
+
+                $theme = $card->themes[0];
+                $theme->html = $html;
+                $theme->save();
+            }
+        }
         echo "Cards seeded.\n";
     }
 }
