6c97c1b5aea0d93a59532fd3b201af84a3f8aac6
Neoscripter <sange0337@gmail.com>
2026-09-06 20:58:41 +0800

    Refactor:create the new version of CardFactory

--- php/db/Factories/CardFactory.php
+++ php/db/Factories/CardFactory.php
@@ -4,36 +4,25 @@ declare(strict_types=1);
 
 namespace Factories;
 
-use Enums\CardVariant;
-use Enums\ImageableType;
 use Http\Models\Card;
 
 class CardFactory extends Factory
 {
-    public function create(?array $attrs = [], ?bool $with_back = false)
+    public function create(array $attrs, string $img_src)
     {
         $card = new Card();
-
-        $card->name = $attrs['name'] ?? $this->faker->word();
-        $card->advice = $attrs['advice'] ??  $this->faker->sentence();
-
-        $card->description = $attrs['description'] ?? $this->faker->sentences(20, true);
-        $card->variant = $attrs['variant'] ?? CardVariant::METAPHORIC->value;
+        $card->copyfrom($attrs);
         $card->save();
 
-        $imageable_type = ImageableType::from($card->variant)->value;
+        $img_attrs = [
+            'imageable_type' => $attrs['variant'],
+            'imageable_id' => $card->id,
+        ];
 
         (new ImageFactory)->create(
-            attrs: ['imageable_type' => $imageable_type, 'imageable_id' => $card->id, 'variant' => 'front_image'],
-            src_dir: APP_DIR . '/db/Fixtures/Image/front_image/',
+            attrs: array_merge($img_attrs, ['variant' => 'front_image']),
+            src_dir: $img_src
         );
-
-        if ($with_back) {
-            (new ImageFactory)->create(
-                attrs: ['imageable_type' => $imageable_type, 'imageable_id' => $card->id, 'variant' => 'back_image'],
-                src_dir: APP_DIR . '/db/Fixtures/Image/back_image/',
-            );
-        }
 
         return $card;
     }
