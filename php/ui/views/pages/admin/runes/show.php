c5ae4f0c9be24d45dacbf3b8973e3a8cefac0637
Neoscripter <sange0337@gmail.com>
2026-09-07 16:16:32 +0800

    Style:adjust the rune theme hmtl text to the themes

--- php/ui/views/pages/admin/runes/show.php
+++ php/ui/views/pages/admin/runes/show.php
@@ -66,7 +66,7 @@ $back_img_alt = $rune['back_image']['alt'] ?? '';
                             <h4 class="mb-6 font-medium">
                                 <?= $theme->name ?>
                             </h4>
-                            <div class="max-w-full prose prose-sm">
+                            <div class="max-w-full prose prose-sm text-foreground">
                                 <?= \Markdown::instance()->convert($theme->html); ?>
                             </div>
                         </div>
