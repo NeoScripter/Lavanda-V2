5534b5fded5cc4733938c99d76c20c55b0f73a11
Neoscripter <sange0337@gmail.com>
2026-09-07 15:21:34 +0800

    Feat:adapt RuneSeeder to the fixtures

--- php/db/Seeders/RuneSeeder.php
+++ php/db/Seeders/RuneSeeder.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Seeders;
 
+use Enums\ThemeableType;
 use Factories\RuneFactory;
+use Http\Models\Theme;
+use InvalidArgumentException;
 use Seeders\Seeder;
 
 class RuneSeeder extends Seeder
@@ -16,9 +19,59 @@ class RuneSeeder extends Seeder
             return;
         }
 
+        $source = APP_DIR . '/db/Fixtures/Rune/';
+
         $factory = new RuneFactory();
-        for ($i = 0; $i < 10; $i++) {
-            $factory->create();
+
+        $fixtures = array_filter(
+            scandir($source),
+            fn($dir) => $dir !== '..' && $dir !== '.' && is_dir($source . '/' . $dir)
+        );
+
+        if (empty($fixtures)) {
+            throw new InvalidArgumentException("Rune fixtures are not created");
+        }
+
+        foreach ($fixtures as $fixture) {
+            $seed_dir = remove_extra_slashes($source . '/' . $fixture . '/');
+
+            $name = read_or_throw($seed_dir . 'name.txt', "Couldn't extract rune name from the file");
+            $advice = read_or_throw($seed_dir . 'advice.txt', "Couldn't extract rune advice from the file");
+
+            $rune = $factory->create(
+                attrs: compact('name', 'advice', 'description', 'variant'),
+                front_img_src: $seed_dir . 'front_image/',
+                back_img_src: $seed_dir . 'back_image/',
+            );
+
+            $theme_fixtures = array_filter(
+                scandir($seed_dir . 'themes'),
+                fn($dir) => $dir !== '..' && $dir !== '.' && is_dir($dir)
+            );
+
+            if (empty($theme_fixtures)) {
+                throw new InvalidArgumentException("Rune theme fixtures are not created");
+            }
+
+            $rune->themes[0]->erase();
+
+            foreach ($theme_fixtures as $theme_fixture) {
+                $theme_dir = remove_extra_slashes($seed_dir . '/' . $theme_fixture . '/');
+
+                $name = read_or_throw($theme_dir . 'name.txt', "Couldn't extract theme name from the file");
+                $html = read_or_throw($theme_dir . 'html.md', "Couldn't extract theme html from the file");
+
+                $theme_attrs = [
+                    'name' => $name,
+                    'themeable_type' => ThemeableType::RUNE->value,
+                    'themeable_id' => $rune->id,
+                    'html' => $html
+                ];
+
+                $theme = new Theme();
+                $theme->copyfrom($theme_attrs);
+                $theme->save();
+            }
         }
 
         echo "Runes seeded.\n";
