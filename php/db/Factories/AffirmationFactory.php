970655482a82f44d652ad5b7367a472c9d710511
Neoscripter <sange0337@gmail.com>
2026-09-07 15:27:01 +0800

    Feat:adapt AffirmationSeeder and AffirmationFactory to the fixtures

--- php/db/Factories/AffirmationFactory.php
+++ php/db/Factories/AffirmationFactory.php
@@ -8,13 +8,10 @@ use Http\Models\Affirmation;
 
 class AffirmationFactory extends Factory
 {
-    public function create(?array $attrs = [])
+    public function create(array $attrs)
     {
         $affirmation = new Affirmation();
-
-        $affirmation->topic = $attrs['topic'] ?? $this->faker->word();
-        $affirmation->quote = $attrs['quote'] ??  $this->faker->sentences(3, true);
-
+        $affirmation->copyfrom($attrs);
         $affirmation->save();
 
         return $affirmation;
--- php/db/Seeders/AffirmationSeeder.php
+++ php/db/Seeders/AffirmationSeeder.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Seeders;
 
 use Factories\AffirmationFactory;
+use InvalidArgumentException;
 use Seeders\Seeder;
 
 class AffirmationSeeder extends Seeder
 {
+
     public static function run()
     {
         if (self::is_seeded(db_table: 'affirmations')) {
@@ -16,14 +18,26 @@ class AffirmationSeeder extends Seeder
             return;
         }
 
-        $factory = new AffirmationFactory();
-        $topics = ['Мама и малыш', 'Карьера, бизнес', 'Гармония'];
+        $source = APP_DIR . '/db/Fixtures/Affirmation/';
 
-        foreach ($topics as $topic) {
-            for ($i = 0; $i < 10; $i++) {
-                $factory->create(compact('topic'));
-            }
-            
+        $fixtures = array_filter(
+            scandir($source),
+            fn($dir) => $dir !== '..' && $dir !== '.' && is_dir($source . '/' . $dir)
+        );
+
+        if (empty($fixtures)) {
+            throw new InvalidArgumentException("Fixtures for affirmations are not created");
+        }
+
+        $factory = new AffirmationFactory();
+
+        foreach ($fixtures as $fixture) {
+            $seed_dir = remove_extra_slashes($source . '/' . $fixture . '/');
+
+            $topic = read_or_throw($seed_dir . 'topic.txt', "Couldn't extract affirmation topic from the file");
+            $quote = read_or_throw($seed_dir . 'quote.txt', "Couldn't extract affirmation quote from the file");
+
+            $factory->create(attrs: compact('topic', 'quote'));
         }
 
         echo "Affirmations seeded.\n";
