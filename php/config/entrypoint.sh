90fdf2ae5eeed5362516c828f8398140cba444bd
Neoscripter <sange0337@gmail.com>
2026-09-08 10:51:48 +0800

    Fix:remove migration and seed scripts from the dockerfile entrypoint

--- php/config/entrypoint.sh
+++ php/config/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 chown -R www-data:www-data /var/www/html/storage/public
 
-su-exec www-data php /var/www/html/public/index.php migrate
-su-exec www-data php /var/www/html/public/index.php seed
-su-exec www-data php /var/www/html/public/index.php create_user --name=Ilya --email=sange0337@gmail.com --password=password
-
 exec "$@"
