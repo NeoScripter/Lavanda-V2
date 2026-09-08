0795cdf99799ea54e331291d7a40a4416cdefa33
Neoscripter <sange0337@gmail.com>
2026-09-08 16:50:33 +0800

    Fix:adjust the match set table view to select only front_image and preview variants

--- php/app/Http/Controllers/CliController.php
+++ php/app/Http/Controllers/CliController.php
@@ -322,11 +322,11 @@ class CliController
                         'src', image.src,
                         'alt', image.alt
                     )
-                ) FILTER (WHERE image.id IS NOT NULL), '[]'
+                ) FILTER (WHERE image.id IS NOT NULL AND (image.variant = 'front_image' OR image.variant = 'preview')), '[]'
             ) as images
             FROM match_sets set
             LEFT JOIN images image ON image.imageable_id = ANY(STRING_TO_ARRAY(set.matcheable_id, '|')::int[]) AND image.imageable_type = set.matcheable_type
-            GROUP BY set.id, set.matcheable_id, set.matcheable_type, set.locale, set.created_at;"
+            GROUP BY set.id, set.advice, set.matcheable_id, set.matcheable_type, set.locale, set.created_at;"
         );
     }
 
