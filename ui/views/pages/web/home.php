7d991e71fae7897179ec8448dd007f7a648b2983
Neoscripter <sange0337@gmail.com>
2026-09-16 18:45:38 +0800

    Feat:add typography styles

--- ui/views/pages/web/home.php
+++ ui/views/pages/web/home.php
@@ -5,7 +5,7 @@
 
 <div class="p-2">
     <div>Hell 2o 9 world</div>
-    <div>Привет мир</div>
+    <h1>Привет мир</h1>
     <?=  /* component('web/ui/button', ['slot' => 'hello world']) */ '' ?>
     <?php slot('components/web/ui/button', ['variant' => 'outline', 'size' => 'md']); ?>
     hello world
