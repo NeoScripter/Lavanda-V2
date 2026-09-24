670205632c0939e1b28a90874bfb848d7218a42c
Neoscripter <sange0337@gmail.com>
2026-09-24 11:41:11 +0800

    Refactor:extract practice item content into a separate component

--- ui/views/pages/web/practice.php
+++ ui/views/pages/web/practice.php
@@ -32,7 +32,6 @@ extract(component_props(
 
 <span></span>
 
-<?php $s_item = $items[2]->to_resource(); ?>
 <!-- Items -->
 <section>
     <?php if (! empty($items)) : ?>
@@ -42,32 +41,7 @@ extract(component_props(
                 <?= component('web/ui/practice-item-card', compact('item')) ?>
             <?php endforeach; ?>
 
-            <li class='col-start-1 -col-end-1 xl:-mx-[calc(var(--px-lg)/2)] xs:-mx-(--px-lg) isolate relative before:absolute before:-z-1 py-7 sm:py-10 lg:py-12 2xl:py-13 before:bg-background-muted before:inset-y-0 before:left-1/2 before:-translate-x-1/2 before:w-screen flex flex-col gap-12 sm:gap-14 lg:gap-23 lg:flex-row 2xl:gap-49 lg:justify-between'>
-                <?= component('shared/ui/image', [
-                    'sizes'    => 'mb|tb',
-                    'alt' => $item['image_alt'],
-                    'path'     => $item['image_src'],
-                    'prt_class' => '!bg-contain lg:order-1 shrink-0 lg:max-w-1/2 xl:max-w-168 xl:w-full',
-                    'img_class' => 'size-full object-contain!',
-                ]) ?>
-                <div class='space-y-6 lg:space-y-8'>
-                    <h3 class='sm:text-left'><?= $item['title'] ?></h3>
-                    <p class='text-center text-balance sm:text-left'><?= $item['description'] ?></p>
-
-                    <?php if ($item['file']) : ?>
-                        <?= component('web/ui/file-link', ['url' => $item['file'], 'label' => 'Скачать файл', 'class' => 'mx-auto sm:mx-0 mb-[0.75em]']) ?>
-                    <?php endif; ?>
-
-                    <div>
-                        <?php foreach ($s_item['faqs'] as $idx => $faq) : ?>
-                            <details name="faqs" <?= $idx === 0 ? 'open' : '' ?> class='border-b border-gray-400 py-[1em] px-[0.5em]'>
-                                <summary class='cursor-pointer font-semibold'><?= ($idx + 1) . '. ' . $faq['question'] ?></summary>
-                                <p class='py-[1em]'><?= $faq['answer'] ?></p>
-                            </details>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-            </li>
+            <?= component('web/ui/practice-item-content', ['item' => $items[0]]) ?>
 
             <?php if (count($items) > 2) : ?>
                 <?php foreach (array_slice($items, 2) as $item) : ?>
